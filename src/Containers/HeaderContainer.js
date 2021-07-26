14643e8c3fd9f588c0ed700b99cc73b41334cae1
Дмитрий Карелин <d.v.karel1n@mail.ru>
2021-07-26 16:14:08 +0300

    header ready

---
+++ src/Containers/HeaderContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Header from "../Views/Header";
+
+const HeaderContainer = () => {
+  return <Header isAuthorized={true} />;
+};
+
+export default HeaderContainer;
---
+++ src/Views/AccountMenu/styles.js
@@ -0,0 +1,30 @@
+import { alpha, makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  menu: {
+    "& .MuiPaper-root": {
+      backgroundColor: "#000",
+    },
+  },
+  menuItem: {
+    backgroundColor: "#000",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.2),
+    },
+  },
+  flex: {
+    display: "flex",
+  },
+  title: {
+    display: "none",
+    alignSelf: "center",
+    margin: "0 10px",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+    color: "#fff",
+  },
+}));
+
+export default useStyles;
