60768aefdf3ae761fee70a074010786b318628d4
Aliaksei Barushka <alekseyknows@gmail.com>
2021-07-29 15:09:56 +0300

    added filmList selector

--- src/Containers/FilmCardListContainer.js
+++ src/Containers/FilmCardListContainer.js
@@ -2,10 +2,11 @@ import { FilmCardList } from '../Views/FilmCardList';
 import { useEffect} from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { filmListRequest } from "../redux/actions";
+import { filmListSelector } from '../redux/selectors';
 
 const FilmCardListContainer = () => {
     const dispatch = useDispatch();
-    const {films} = useSelector(state => state.filmList);
+    const {films} = useSelector(filmListSelector);
 
     useEffect(() => {
         dispatch(filmListRequest());
