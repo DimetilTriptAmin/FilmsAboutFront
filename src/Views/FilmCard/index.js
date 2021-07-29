3bc2473016331fad8f02eb7221486aafe67a7235
Дмитрий Карелин <d.v.karel1n@mail.ru>
2021-07-29 17:11:28 +0300

    film posters ready

--- src/Views/FilmCard/index.js
+++ src/Views/FilmCard/index.js
@@ -6,35 +6,36 @@ import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import fight_club_image from "../../static/images/cards/fight_club.jpg";
 import { useStyles } from "./styles";
 
 const FilmCard = (props) => {
   const classes = useStyles();
-
+  console.log(props.poster);
   return (
-    <Card className={classes.root}>
-      <CardHeader
-        className={classes.header}
-        title={props.title}
-        subheader={
-          <Typography
-            className={classes.rating}
-          >{`Rating: ${props.rating}`}</Typography>
-        }
-      />
-      <CardMedia className={classes.media} image={fight_club_image} />
-      <CardContent className={classes.cardContent}>
-        <Typography variant='body2' component='div'>
-          {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button className={classes.button} onClick={props.handleClick}>
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    <div>
+      <Card className={classes.root}>
+        <CardHeader
+          className={classes.header}
+          title={props.title}
+          subheader={
+            <Typography
+              className={classes.rating}
+            >{`Rating: ${props.rating}`}</Typography>
+          }
+        />
+        <CardMedia className={classes.media} image={props.poster} />
+        <CardContent className={classes.cardContent}>
+          <Typography variant='body2' component='div'>
+            {props.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button className={classes.button} onClick={props.handleClick}>
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 };
 
