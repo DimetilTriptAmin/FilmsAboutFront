58d5aa7f51b609afa1a675166d1935166a7b924a
Дмитрий Карелин <d.v.karel1n@mail.ru>
2021-08-05 18:36:20 +0300

    no message

--- src/redux/sagas/callMethods.js
+++ src/redux/sagas/callMethods.js
@@ -5,3 +5,19 @@ import { axiosDefault } from "../../axios";
 export function* filmListRequest() {
   return yield call(axiosDefault, `https://localhost:44364/api/Film`, "get");
 }
+
+export function* commentsRequest(payload) {
+  return yield call(
+    axiosDefault,
+    `https://localhost:44364/api/Comment/forFilm${payload}`,
+    "get",
+  );
+}
+
+export function* filmRequest(payload) {
+  return yield call(
+    axiosDefault,
+    `https://localhost:44364/api/Film/${payload}`,
+    "get",
+  );
+}
