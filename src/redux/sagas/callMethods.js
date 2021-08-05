146ba233e98e3c83acfd56ce1150713dda818e89
Aliaksei Barushka <alekseyknows@gmail.com>
2021-08-05 18:36:47 +0300

    added user sagas to callMethod

--- src/redux/sagas/callMethods.js
+++ src/redux/sagas/callMethods.js
@@ -1,7 +1,43 @@
-import { call } from "@redux-saga/core/effects";
+import { call } from '@redux-saga/core/effects';
 
-import { axiosDefault } from "../../axios";
+import { axiosDefault } from '../../axios';
 
 export function* filmListRequest() {
-  return yield call(axiosDefault, `https://localhost:44364/api/Film`, "get");
+    return yield call(axiosDefault, `https://localhost:44364/api/Film`, 'get');
+}
+
+export function* userByIdRequest(data) {
+    return yield call(
+        axiosDefault,
+        `https://localhost:44364/api/User/${data.payload}`,
+        'get'
+    );
+}
+
+export function* logInRequest(data) {
+    return yield call(
+        axiosDefault,
+        `https://localhost:44364/api/User/login`,
+        'post',
+        JSON.stringify(data.payload)
+    );
+}
+
+export function* registrationRequest(data) {
+    return yield call(
+        axiosDefault,
+        `https://localhost:44364/api/User/register`,
+        'post',
+        JSON.stringify(data.payload)
+    );
+}
+
+export function* logOutRequest() {
+    return yield call(
+        axiosDefault,
+        `https://localhost:44364/api/User/logout`,
+        'delete',
+        null,
+        window.localStorage.getItem('accessToken')
+    );
 }
